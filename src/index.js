60bc3d3777fb5b7a144eeccb2d210950b68288ff
Jess2D <jessicap_silveira@hotmail.com>
2020-08-28 15:59:48 +1200

    Rendering Project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log(movieList);
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const App = () => {
+    return (
+        <div className='container-fluid'>
+            <nav className='navbar sticky-top navbar-light bg-dark'>
+                <h1 className='navbar-brand text-light'>MovieList</h1>
+            </nav>
+
+            <List />
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
