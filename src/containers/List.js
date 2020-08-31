935e1eedc807bf2a975e912fe7af2f947a22c029
Jess2D <jessicap_silveira@hotmail.com>
2020-08-31 15:39:27 +1200

    Retriving local data

--- src/containers/List.js
+++ src/containers/List.js
@@ -1,7 +1,44 @@
 import React, { Component } from 'react';
+import data from '../assets/data.json';
+import Card from '../components/Card';
+import CardColumns from 'react-bootstrap/CardColumns';
+
 class List extends Component {
-    render() {
-        return <h1>movieList</h1>;
+    constructor() {
+        super();
+        this.state = {
+            data: [],
+            loading: true
+        }
     }
-};
-export default List;
+
+    async componentDidMount() {
+        const movies = data.map((x) => x);
+        if (movies.length > 1) {
+            this.setState({
+                data: movies,
+                loading: false,
+            });
+        }
+    }
+
+    render() {
+        const { data, loading } = this.state;
+
+        if (loading) {
+            return <div>Loading...</div>;
+        }
+
+        return (
+            <div className='row'>
+                {data.map(movie => (
+                    <div key={movie.id} className='col-sm-2'>
+                        <Card movie={movie} />
+                    </div>
+                ))}
+            </div>
+        );
+    };
+
+}
+export default List
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import List from './containers/List'
+
+
 
 const App = () => {
     return (
         <div className='container-fluid'>
             <nav className='navbar sticky-top navbar-light bg-dark'>
-                <h1 className='navbar-brand text-light'>MovieList</h1>
+                <h1 className='navbar-brand text-light'>Movie List</h1>
             </nav>
-
             <List />
         </div>
+
     );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
